e3d3864b0946bc3b58cf388a08ad46c41f3fae61
Samuel Simões <samuelsimoes31@gmail.com>
2025-05-18 15:21:37 -0300

    Refactor pretty_matrix_multiplication function into utils.py and update atv1_sssf2_tcsr.py to import it

--- atv1_sssf2_tcsr.py
+++ atv1_sssf2_tcsr.py
@@ -1,33 +1,6 @@
 import numpy as np
 import math as mt
-
-def pretty_matrix_multiplication(A, H, P):
-    A = np.array(A).reshape(-1, 1)
-    H = np.array(H)
-    P = np.array(P).reshape(-1, 1)
-
-    output = ""
-    for i in range(H.shape[0]):
-        line = ""
-
-        # Matriz H
-        line += "| " + " ".join(f"{H[i, j]:>6.3f}" for j in range(H.shape[1])) + " |"
-
-        # Multiplicador
-        line += "   *" if i == 1 else "    "
-
-        # Matriz P
-        line += "   | {:>6.3f} |".format(P[i, 0])
-
-        # Igualdade e resultado
-        line += "  =" if i == 1 else "   "
-        line += "  | {:>7.3f} |".format(A[i, 0])
-
-        output += line + "\n"
-
-    print(output)
-
-
+from utils import pretty_matrix_multiplication
 
 def SE2_xy(Dx,Dy):
     return np.array([
