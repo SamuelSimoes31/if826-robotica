8e6cf3e3d697c1b72d928ee77b0fc7882cdd57e3
samuelsimoes31 <samuelsimoes31@gmail.com>
2025-06-09 12:04:24 -0300

    bibiboboo

--- kinematics.py
+++ kinematics.py
@@ -12,6 +12,13 @@ def fk(theta1,theta2):
     theta = mt.atan2(y, x)
     return x , y, mt.degrees(theta)
 
+def ik(x,y):
+    # se o ponto está dentro da área de atuação
+    # se ele tiver exatamente na borda da área de atuação (1 resposta)
+    # se ele está dentro da da borda (2 respostas)
+
+    return theta1, theta2
+
 def main():
     print(fk(0,0))
     print(fk(mt.pi/2, 0))
@@ -20,3 +27,19 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# ele consegue chegar na velcodiade maxima?
+
+# tempo necessario pra chegar na vel max
+# distancia perc nesse tempo.
+
+# enviar a cada ts a posição q ele precisa atingir
+
+# os dois acabar no mesmo momento
+# uma solução pro 1 motor, 1 solução pro 2º motor. Pegar o mais lento e adapatar o mais rpaido pra ficar mias lento.
+# Pra isso, via mudar a velocidade máxima pra ser mais baixa.
+
+# 1dof_traj(theta, theta, vmax, amax, ts)
+
+# dof_time(ts)
