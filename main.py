ffdd8f47835d9b0831821b9104a1f98f2846045d
Samuel Simões <samuelsimoes31@gmail.com>
2025-06-29 19:00:05 -0300

    adj

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from utils import plot_trajectory_euclidean, plotar_series_temporais_completo
 robot = rtb.models.DH.Planar2()
 
 # trajetória no espaço de configuração
-q_traj_eucl, pos_traj, t_traj = traj_eucl(0, -1.5, 0.4, 0, ds=0.01)
-plot_trajectory_euclidean(pos_traj, q_traj_eucl, t_traj)
+q_traj_eucl, pos_traj_eucl, t_traj_eucl = traj_eucl(0, -1.5, 0.4, 0, ds=0.01)
+plot_trajectory_euclidean(pos_traj_eucl, q_traj_eucl, t_traj_eucl)
 robot.plot(q_traj_eucl, backend='pyplot', movie='cartesiano_eucl.gif', dt=0.01)
 
 # trajetória no espaço das juntas
